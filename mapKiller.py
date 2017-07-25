9819a322df3828f5df535dc8224c299390af22d6
dandes3 <hopeisforsissies@icloud.com>
2017-07-25 18:58:58 -0400

    Initial file written

---
+++ mapKiller.py
@@ -0,0 +1,8 @@
+# ---------------------------------------
+# Created by Don Andes and Dan Sciortino |
+# ---------------------------------------------------------------------------------------------------
+# This program utilizes a web driver and machine learning to pull an image from missing maps and 
+# pattern match basic structures onto a coordinate system. 
+#
+# Hopefully.  
+# ---------------------------------------------------------------------------------------------------
